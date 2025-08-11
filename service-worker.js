e3b1959e6370da5c4f9a7230f7dd6dcbeda76746
S1lv3rPlum <Reneeslueke@gmail.com>
2025-08-11 07:13:10 -0400

    update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -24,6 +24,32 @@ self.addEventListener("activate", event => {
   );
 });
 
+const CACHE_NAME = "filament-cache-v3"; // change version to force update
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/home.js",
+  "/home",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
